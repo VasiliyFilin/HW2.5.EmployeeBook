e58f3c38bd8fa2b4e0fc71a979ddb33a48e9e053
Vasily Filin <pro.vasiliy.filin@gmail.com>
2024-01-09 15:45:09 +0300

    HW2.5.Collections.EmployeeBook refactor init

---
+++ src/main/java/pro/sky/employeebook/SkyproEmployeebookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproEmployeebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproEmployeebookApplication.class, args);
+	}
+
+}
